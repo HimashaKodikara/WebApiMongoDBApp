3192c4163e9ffb3dec9d32cda2b40ec3106d0da0
19840769 <kodikarah.21@uom.lk>
2024-06-10 15:33:47 +0530

    chore: Update Edit component to include form data prefill for editing a student

--- WebApiMongoDB/ClientApp/src/components/Edit.js
+++ WebApiMongoDB/ClientApp/src/components/Edit.js
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+
 const entry = {
     id: "",
     firstName: "",
@@ -10,28 +12,71 @@ const entry = {
     age:0
 }
 export default function Edit(props) {
+
+    const [data,setData] = useState([]); 
+    const [gender_,setGender_] = useState([]); 
+    const [graduated,setGraduated] = useState([]); 
+    const UpdateStudent = ()=>{
+        console.log("The new student is:",entry);
+        fetch("api/student",{
+            method:"PUT",
+            body:JSON.stringify(entry),
+            headers:{
+                "Content-Type":"application/json"	
+            }
+        }).then(r=>{
+            console.log("Response for updating a student :",r);
+            window.location="/"
+        }).catch(e=>console.log("Error updating a student",e));
+    }
+    const newData = (e)=>{
+        const name_ = e.target.name;
+        let v_ = e.target.value
+
+        if(name_ ==='dateOfBirth'){
+            v_ = new Date(v_);
+            entry.age = new Date().getFullYear() - v_.getFullYear();
+        }
+        if(name_ === 'gender'){
+            v_ = Number(v_);
+        }
+        if(name_ === 'isGraduated'){
+            v_ = v_ === '1' ? true : false;
+        }
+        entry[name_] = v_
+
+        console.log("The New Student Is:",entry)
+    }
+    useEffect(()=>{
+        fetch("api/student/"+id).then(r=> r.json()).then(d =>{
+            console.log("The student for update is:",d)
+            setData(d)
+            setGender_(d.gender)
+            setGraduated(d.isGraduated)
+        }).catch(e=>console.log("Error getting student for update",e))
+    })
     return(
 <section className="m-20">
     <h1>Update Student</h1>
     <div className="mt-10">
         <label htmlFor="fn">First Name</label>
-        <input type="text" name="firstName" id="fn" onChange={newData}/>
+        <input type="text" name="firstName" id="fn" defaultValue={data.firstName} onChange={newData}/>
     </div>
     <div className="mt-10">
         <label htmlFor="ln">Last Name</label>
-        <input type="text" name="lastName" id="ln" onChange={newData}/>
+        <input type="text" name="lastName" id="ln" defaultValue={data.lastName} onChange={newData}/>
     </div>
     <div className="mt-10">
         <label htmlFor="cn">Class Name</label>
-        <input type="text" name="className" id="cn" onChange={newData}/>
+        <input type="text" name="className" id="cn" defaultValue={data.className} onChange={newData}/>
     </div>
     <div className="mt-10">
         <label htmlFor="dp">Department</label>
-        <input type="text" name="department" id="dp" onChange={newData}/>
+        <input type="text" name="department" id="dp" defaultValue={data.department} onChange={newData}/>
     </div>
     <div className="mt-10">
         <label htmlFor="gender">Gender</label>
-        <select name="gender" id="gender" onChange={newData}>
+        <select name="gender" id="gender" value={} onChange={newData}>
             <option value={1}>Male</option>
             <option value={0}>Female</option>
         </select>
@@ -51,7 +96,7 @@ export default function Edit(props) {
     </div>
     <div className="mt-30 row p20 justify-btw">
         <div className="btn cancel" onClick={()=> window.location = "/"}>Cancel</div>
-        <div className="btn add" onClick={addNewStudent}>Add</div>
+        <div className="btn add" onClick={UpdateStudent}>Add</div>
     </div>
 </section>
     );
